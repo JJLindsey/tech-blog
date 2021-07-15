ea8aae0315d65ed1856f91fd9c5c5ca48a8e627f
JJLindsey <jlindsey010@gmail.com>
2021-07-15 10:54:38 -0700

    create user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,127 @@
+//create 5 routes that work w/ User model to perform CRUD operations
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//GET /api/users
+router.get('/', (req, res) => {
+    //access our User model & run .findAll() method
+    User.findAll({
+        attributes: { excluse: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//GET /api/users/1
+router.get('/:id', (req, res) => {
+    // like sql query SELECT * FROM users WHERE id = 1
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+//POST /api/users
+router.post('/', (req, res) => {
+    //expects {username: 'Lerantino', email: 'lernantinit@gmail.com', password: 'password'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/login', (req, res) => {
+    //query operation
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in.' });
+
+    });
+});
+
+//PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+    //pass in req.body instead to only update what's passed through
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ meesage: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json.apply(err)
+        });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const session = require('express-session')
+
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
