6ab70f948be782dbfcdb813ae344c1d566426cb6
JJLindsey <jlindsey010@gmail.com>
2021-07-23 16:46:26 -0700

    update server.js and add login.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,41 @@
 //user-facing routes ie homepage and login page
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user'
-        }
-
-    });
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comments', 'post_id', 'user_id', 'date_created'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,17 +11,30 @@ const app = express();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 // The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets.
 //This is useful for front-end specific files like images, style sheets, and JavaScript files.
 app.use(express.static(path.join(__dirname, 'public'))),
 
-// turn on routes
-app.use(routes);
+    // turn on routes
+    app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
