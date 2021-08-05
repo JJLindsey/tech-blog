b1cefd0166ccd2d02535586ef606f5c03f1c2a73
JJLindsey <jlindsey010@gmail.com>
2021-08-05 13:02:17 -0700

    add delete and edit functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
+
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-const apiRoutes = require('./api');
 
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
-router.use('./', homeRoutes);
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 
 module.exports = router;
